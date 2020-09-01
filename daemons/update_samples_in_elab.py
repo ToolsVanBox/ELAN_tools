7db14bde6340e6e0e235e69c794331262dfd6d47
mroosmalen <m.j.vanroosmalen-3@prinsesmaximacentrum.nl>
2020-09-01 14:56:22 +0200

    Add user to log

--- daemons/update_samples_in_elab.py
+++ daemons/update_samples_in_elab.py
@@ -3,10 +3,10 @@ import sys
 from datetime import datetime
 import getpass
 
-def updateSamples( elan, samplenames, key, force ):
+def updateSamples( elan, samplenames, key, force, add ):
     elan_samples = elan.get_samples_for_names( samplenames.keys() )
     for sample in elan_samples:
-        if not force:
+        if not force and not add:
             if next((item for item in sample['meta'] if item['key'] == key and item['value'] != ''), False):
                 continue
         meta_fields = elan.get_meta_field_from_sample_type_by_key(sample['sampleType']['sampleTypeID'], key)
@@ -14,7 +14,15 @@ def updateSamples( elan, samplenames, key, force ):
         meta_params['key'] = meta_fields[0]['key']
         meta_params['sampleTypeMetaID'] = meta_fields[0]['sampleTypeMetaID']
         meta_params['sampleDataType'] = meta_fields[0]['sampleDataType']
-        meta_params['value'] = samplenames[sample['name']]
+        if add:
+            sample_meta_fields = elan.get_sample_meta_fields_by_key(sample['sampleID'], key )
+            values = sample_meta_fields[0]['value'].split("\n")
+            if samplenames[sample['name']] not in values:
+                meta_params['value'] = sample_meta_fields[0]['value'] + "\n" + samplenames[sample['name']]
+            else:
+                continue
+        else:
+            meta_params['value'] = samplenames[sample['name']]
         response = elan.put_meta(sample['sampleID'], meta_params)
         if response == 200:
             username = getpass.getuser()
@@ -22,21 +30,21 @@ def updateSamples( elan, samplenames, key, force ):
             study_name = next((item['value'] for item in sample['meta'] if item['key'] == 'Study Name'))
             print( "".join([str(datetime.now()),"\tUPDATE SAMPLE\tUser=",username,";Sample=", sample['name'], ";Project=", project_id, ";Study=",study_name,";",meta_params['key'],"=",meta_params['value']]) )
 
-def updateSampleWithRawData( elan, raw_data, force ):
+def updateSampleWithRawData( elan, raw_data, force, add ):
     samplenames = {}
     for fastq in Path(raw_data).rglob('*.fastq.gz'):
         filename = Path(fastq).name
         samplename = filename.split("_")[0]
         samplenames[samplename] = str(Path(fastq).parent)
-    updateSamples( elan, samplenames, "Raw Data", force )
+    updateSamples( elan, samplenames, "Raw Data", force, add )
 
-def updateSampleWithProcessedData( elan, processed_data, force ):
+def updateSampleWithProcessedData( elan, processed_data, force, add ):
     samplenames = {}
     for bam in Path(processed_data).rglob('*dedup.bam'):
         filename = Path(bam).name
         samplename = filename.split("_")[0]
         samplenames[samplename] = str(Path(bam).parent)
-    updateSamples( elan, samplenames, "Processed Data", force )
+    updateSamples( elan, samplenames, "Processed Data", force, add )
 
 def updateSampleWithDataBackup( elan, data_backup, force ):
     samplenames = {}
@@ -44,9 +52,9 @@ def updateSampleWithDataBackup( elan, data_backup, force ):
         filename = Path(bam).name
         samplename = filename.split("_")[0]
         samplenames[samplename] = str(Path(bam).parent)
-    updateSamples( elan, samplenames, "Data Backup", force )
+    updateSamples( elan, samplenames, "Data Backup", force, add )
 
-def updateSampleWithDataAnalysis( elan, data_analysis, project_name, study_names, sample_names, force ):
+def updateSampleWithDataAnalysis( elan, data_analysis, project_name, study_names, sample_names, force, add ):
     samplenames = {}
     if study_names:
         for study_name in study_names:
@@ -56,9 +64,9 @@ def updateSampleWithDataAnalysis( elan, data_analysis, project_name, study_names
     if sample_names:
         for sample_name in sample_names:
             samplenames[sample_name] = data_analysis
-    updateSamples( elan, samplenames, "Data Analysis", force )
+    updateSamples( elan, samplenames, "Data Analysis", force, add )
 
-def updateSampleWithSurfdrive( elan, data_analysis, project_name, study_names, sample_names, force ):
+def updateSampleWithSurfdrive( elan, data_analysis, project_name, study_names, sample_names, force, add ):
     samplenames = {}
     if study_names:
         for study_name in study_names:
@@ -68,19 +76,21 @@ def updateSampleWithSurfdrive( elan, data_analysis, project_name, study_names, s
     if sample_names:
         for sample_name in sample_names:
             samplenames[sample_name] = data_analysis
-    updateSamples( elan, samplenames, "Surfdrive", force )
+    updateSamples( elan, samplenames, "Surfdrive", force, add )
 
 def run(elan, args):
     if not ( args.raw or args.processed or args.analysis or args.backup or args.surfdrive):
         sys.exit("No action requested, add --raw, --processed, --analysis, --surfdrive and/or --backup")
     if ((args.analysis or args.surfdrive) and not ( args.project and (args.study or args.sample))):
         sys.exit("No action requested, add --project and --study or --sample together with --analysis and/or --surfdrive")
+    if ( args.add and not (args.analysis or args.surfdrive)):
+        sys.exit("No action requested, add --analysis and/or --study together with --add")
     if args.raw:
-        updateSampleWithRawData( elan, args.raw, args.force )
+        updateSampleWithRawData( elan, args.raw, args.force, args.add )
     if args.processed:
-        updateSampleWithProcessedData( elan, args.processed, args.force )
+        updateSampleWithProcessedData( elan, args.processed, args.force, args.add )
     if args.backup:
-        updateSampleWithDataBackup( elan, args.backup, args.force )
+        updateSampleWithDataBackup( elan, args.backup, args.force, args.add )
     if args.analysis:
         if args.project:
             args.project = ' '.join(args.project)
@@ -88,7 +98,7 @@ def run(elan, args):
             args.study = list( map(lambda x: ' '.join(x), args.study))
         if args.sample:
             args.sample = list( map(lambda x: ' '.join(x), args.sample))
-        updateSampleWithDataAnalysis( elan, args.analysis, args.project, args.study, args.sample, args.force )
+        updateSampleWithDataAnalysis( elan, args.analysis, args.project, args.study, args.sample, args.force, args.add )
     if args.surfdrive:
         if args.project:
             args.project = ' '.join(args.project)
@@ -96,4 +106,4 @@ def run(elan, args):
             args.study = list( map(lambda x: ' '.join(x), args.study))
         if args.sample:
             args.sample = list( map(lambda x: ' '.join(x), args.sample))
-        updateSampleWithSurfdrive( elan, args.surfdrive, args.project, args.study, args.sample, args.force )
+        updateSampleWithSurfdrive( elan, args.surfdrive, args.project, args.study, args.sample, args.force, args.add )
